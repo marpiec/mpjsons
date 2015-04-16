d7b769ca162e83222de9ae4276420a202db4f5d4
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2015-04-16 22:27:58 +0200

    Singleton Objects are not supported

--- src/test/scala/pl/mpieciukiewicz/mpjsons/SingletonObjectTest.scala
+++ src/test/scala/pl/mpieciukiewicz/mpjsons/SingletonObjectTest.scala
@@ -3,45 +3,48 @@ package pl.mpieciukiewicz.mpjsons
 import org.scalatest.FlatSpec
 import org.scalatest.MustMatchers._
 
-class User(val name: String)
+// For now singleton objects are not supported!
 
-case object UserMarcin extends User("Marcin")
 
-case object UserRegisterMessage
-
-class SingletonObjectSpec extends FlatSpec {
-
-  "Serializer" must "handle singleton Objects" in {
-
-    val userMarcinJson = MPJson.serialize(UserMarcin)
-
-    userMarcinJson mustBe "{}"
-
-    val deserializedUserMarcin = MPJson.deserialize[AnyRef](userMarcinJson)
-
-    deserializedUserMarcin mustBe UserMarcin
-
-    MPJson.deserialize[User]("{}")
-
-    deserializedUserMarcin mustBe UserMarcin
-
-    try {
-      MPJson.deserialize[User]( """{"name":"Marcin"}""")
-      fail("Expected Exception")
-    } catch {
-      case _:Exception => ()
-    }
-  }
-
-  "Serializer" must "handle singleton Objects without value" in {
-
-    val message = MPJson.serialize(UserRegisterMessage)
-
-    message mustBe "{}"
-
-    val deserializedMessage = MPJson.deserialize[AnyRef](message)
-
-    deserializedMessage mustBe UserRegisterMessage
-  }
-
-}
+//class User(val name: String)
+//
+//case object UserMarcin extends User("Marcin")
+//
+//case object UserRegisterMessage
+//
+//class SingletonObjectSpec extends FlatSpec {
+//
+//  "Serializer" must "handle singleton Objects" in {
+//
+//    val userMarcinJson = MPJson.serialize(UserMarcin)
+//
+//    userMarcinJson mustBe "{}"
+//
+//    val deserializedUserMarcin = MPJson.deserialize[AnyRef](userMarcinJson)
+//
+//    deserializedUserMarcin mustBe UserMarcin
+//
+//    MPJson.deserialize[User]("{}")
+//
+//    deserializedUserMarcin mustBe UserMarcin
+//
+//    try {
+//      MPJson.deserialize[User]( """{"name":"Marcin"}""")
+//      fail("Expected Exception")
+//    } catch {
+//      case _:Exception => ()
+//    }
+//  }
+//
+//  "Serializer" must "handle singleton Objects without value" in {
+//
+//    val message = MPJson.serialize(UserRegisterMessage)
+//
+//    message mustBe "{}"
+//
+//    val deserializedMessage = MPJson.deserialize[AnyRef](message)
+//
+//    deserializedMessage mustBe UserRegisterMessage
+//  }
+//
+//}

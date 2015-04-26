21261b591da0419d1bda3c3285fa629b6879d444
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2015-04-26 12:48:26 +0200

    Added new public methods for different ways of passing type

--- src/main/scala/pl/mpieciukiewicz/mpjsons/MPJsons.scala
+++ src/main/scala/pl/mpieciukiewicz/mpjsons/MPJsons.scala
@@ -1,5 +1,6 @@
 package pl.mpieciukiewicz.mpjsons
 
+import pl.mpieciukiewicz.mpjsons.impl.util.TypesUtil
 import pl.mpieciukiewicz.mpjsons.impl.{DeserializerFactory, JsonInnerException, SerializerFactory, StringIterator}
 
 import scala.reflect.runtime.universe._
@@ -9,6 +10,46 @@ object MPJsons {
   implicit val serializerFactory = new SerializerFactory
 }
 
+private object ErrorMessageFormatter {
+
+  final val trimmingSize = 40
+
+  def formatDeserializationError(json: String, jsonIterator: StringIterator, e: RuntimeException): String = {
+    val consumed = jsonIterator.debugGetConsumedString
+    val remaining = jsonIterator.debugGetRemainingString
+    val consumedTrimmed = consumed.substring(Math.max(0, consumed.length - trimmingSize), consumed.length)
+    val remainingTrimmed = remaining.substring(0, Math.min(remaining.length, trimmingSize))
+    val consumedPlaceholder = " " * consumedTrimmed.length
+    """
+      |Problem deserializing: $json
+      |Problem: ${e.getMessage}
+      |In this place:
+      |...$consumedTrimmed$remainingTrimmed...
+      | $consumedPlaceholder^""".stripMargin
+  }
+}
+
+class StaticSerializer[T](private val innerSerializer: JsonTypeSerializer[T], tpe: Type){
+  def serialize(obj: T): String = {
+    val json = new StringBuilder()
+    innerSerializer.serialize(obj, json)
+    json.toString()
+  }
+}
+
+class StaticDeserializer[T](private val innerDeserializer: JsonTypeDeserializer[T], tpe: Type){
+  def deserialize(json: String): T = {
+    val jsonIterator = new StringIterator(json)
+    try {
+      innerDeserializer.deserialize(jsonIterator)
+    } catch {
+      case e: RuntimeException =>
+        throw new JsonInnerException(ErrorMessageFormatter.formatDeserializationError(json, jsonIterator, e), e)
+    }
+  }
+
+}
+
 /**
  * Main class of json serialization library.
  * @author Marcin Pieciukiewicz
@@ -16,13 +57,21 @@ object MPJsons {
 class MPJsons {
 import MPJsons._
 
-
   private var extractTypeCache = Map[TypeTag[_], Type]()
+  private var extractTypeFromNameCache = Map[String, Type]()
 
   private def extractType[T](tag: TypeTag[T]): Type = {
     extractTypeCache.getOrElse(tag, {
       val tpe = tag.tpe
       extractTypeCache += tag -> tpe
+      tpe
+    })
+  }
+
+  private def extractTypeFromName[T](typeName: String): Type = {
+    extractTypeFromNameCache.getOrElse(typeName, {
+      val tpe = TypesUtil.getTypeFromClass(Class.forName(typeName))
+      extractTypeFromNameCache += typeName -> tpe
       tpe
     })
   }
@@ -42,23 +91,25 @@ import MPJsons._
       deserializerFactory.getDeserializer(tpe).deserialize(jsonIterator)
     } catch {
       case e: RuntimeException =>
-        val trimmingSize = 40
-        val consumed = jsonIterator.debugGetConsumedString
-        val remaining = jsonIterator.debugGetRemainingString
-        val consumedTrimmed = consumed.substring(Math.max(0, consumed.length - trimmingSize), consumed.length)
-        val remainingTrimmed = remaining.substring(0, Math.min(remaining.length, trimmingSize))
-        val consumedPlaceholder = " " * consumedTrimmed.length
-        throw new JsonInnerException(
-        s"""
-           |Problem deserializing: $json
-           |Problem: ${e.getMessage}
-           |In this place:
-           |...$consumedTrimmed$remainingTrimmed...
-           |   $consumedPlaceholder^
-         """.stripMargin, e)
+        throw new JsonInnerException(ErrorMessageFormatter.formatDeserializationError(json, jsonIterator, e), e)
     }
   }
 
+  def deserialize[T](json: String, typeName: String): T = {
+    deserialize(json, extractTypeFromName(typeName))
+  }
+
+  def getStaticDeserializer[T](implicit tag: TypeTag[T]): StaticDeserializer[T] = {
+    getStaticDeserializer(tag.tpe)
+  }
+
+  def getStaticDeserializer[T](tpe: Type): StaticDeserializer[T] = {
+    new StaticDeserializer[T](deserializerFactory.getDeserializer[T](tpe), tpe)
+  }
+
+  def getStaticDeserializer[T](typeName: String): StaticDeserializer[T] = {
+    getStaticDeserializer(extractTypeFromName(typeName))
+  }
 
   /**
    * Creates json String that represents given object.
@@ -68,13 +119,29 @@ import MPJsons._
   def serialize[T](obj: T)(implicit tag: TypeTag[T]): String = {
    serialize(obj, extractType(tag))
   }
-
-
+  
   def serialize[T](obj: T, tpe: Type): String = {
     val json = new StringBuilder()
     serializerFactory.getSerializer(tpe).serialize(obj, json)
     json.toString()
   }
+
+  def serialize[T](obj: T, typeName: String): String = {
+    serialize(obj, extractTypeFromName(typeName))
+  }
+
+  def getStaticSerializer[T](implicit tag: TypeTag[T]): StaticSerializer[T] = {
+    getStaticSerializer(tag.tpe)
+  }
+
+  def getStaticSerializer[T](tpe: Type): StaticSerializer[T] = {
+    new StaticSerializer[T](serializerFactory.getSerializer[T](tpe), tpe)
+  }
+
+  def getStaticSerializer[T](typeName: String): StaticSerializer[T] = {
+    getStaticSerializer(extractTypeFromName(typeName))
+  }
+
 
   /**
    * Method to register custom json converter to support custom types of data.

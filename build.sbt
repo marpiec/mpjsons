18801d3e0aafdc59d10be5da7eea26935464e647
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2017-12-19 12:28:28 +0100

    Fix for immutable ListMap deserialization

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ name := "mpjsons"
 
 organization := "io.mpjsons"
 
-version := "0.6.20"
+version := "0.6.21"
 
 scalaVersion := "2.11.7"
 
---
+++ src/test/scala/io/mpjsons/ListMapSpec.scala
@@ -0,0 +1,50 @@
+package io.mpjsons
+
+import org.scalatest.FlatSpec
+import org.scalatest.MustMatchers._
+
+import scala.collection.immutable.ListMap
+
+
+case class ListMapEntry(name: String)
+
+
+class ListMapSpec extends FlatSpec {
+
+  val mpjsons = new MPJsons
+
+  "Serializer" must "handle maps" in {
+
+
+    var map = new ListMap[String, ListMapEntry]
+
+    map += "A" -> ListMapEntry("A")
+    map += "E" -> ListMapEntry("E")
+    map += "D" -> ListMapEntry("D")
+    map += "C" -> ListMapEntry("C")
+    map += "B" -> ListMapEntry("B")
+
+
+    val json = mpjsons.serialize(map)
+
+    json mustEqual """[["A",{"name":"A"}],["E",{"name":"E"}],["D",{"name":"D"}],["C",{"name":"C"}],["B",{"name":"B"}]]"""
+  }
+
+
+  "deserializer" must "preserve order" in {
+
+    val json = """[["A",{"name":"A"}],["E",{"name":"E"}],["D",{"name":"D"}],["C",{"name":"C"}],["B",{"name":"B"}]]"""
+
+    var map = mpjsons.deserialize[ListMap[String, ListMapEntry]](json)
+
+    val entries = map.toList
+
+    entries mustEqual List("A" -> ListMapEntry("A"),
+                           "E" -> ListMapEntry("E"),
+                           "D" -> ListMapEntry("D"),
+                           "C" -> ListMapEntry("C"),
+                           "B" -> ListMapEntry("B"))
+
+  }
+
+}

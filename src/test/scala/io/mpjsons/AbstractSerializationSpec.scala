df0c578c27319d2be8a11cbc42a19846cf6adb4b
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2018-01-08 09:25:51 +0100

    Initial test for abstract/typed serialization

---
+++ src/test/scala/io/mpjsons/AbstractSerializationSpec.scala
@@ -0,0 +1,37 @@
+package io.mpjsons
+
+import org.scalatest.FlatSpec
+import org.scalatest.MustMatchers._
+
+object AbstractSerializationSpec {
+
+  trait CommonType
+
+  case class A(a: String) extends CommonType
+  case class B(b: Int) extends CommonType
+
+}
+
+
+class AbstractSerializationSpec extends FlatSpec {
+
+  import AbstractSerializationSpec._
+
+  val mpjsons = new MPJsons
+
+  "Serializer" must "add type information while serializing trait" in {
+
+    val a = A("Hello")
+    val b = B(42)
+
+    val aCommon: CommonType = a
+    val bCommon: CommonType = b
+
+    mpjsons.serialize(a) mustEqual """{"a":"Hello"}"""
+    mpjsons.serialize(b) mustEqual """{"b":42}"""
+    mpjsons.serialize(aCommon) mustEqual """{"A":{"a":"Hello"}}"""
+    mpjsons.serialize(bCommon) mustEqual """{"B":{"b":42}}"""
+
+  }
+
+}
